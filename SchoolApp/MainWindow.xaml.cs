d18be8e83a92ae3fcff7d868d3c8cff4dcad559e
rachelchaya <rachelchayae@gmail.com>
2025-02-15 21:39:28 +0200

    Add project files.

---
+++ SchoolApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StudentsWindow studentsWindow = new StudentsWindow();
+            studentsWindow.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TeachersWindow teacherWindow = new TeachersWindow();
+            teacherWindow.Show();
+        }
+    }
+}
